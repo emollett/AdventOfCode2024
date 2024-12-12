7b0d92e35efbdc760730a75ba4cf0661357b9b9e
Eleanor Mollett <eleanor.mollett@gmail.com>
2024-12-12 09:40:44 +0000

    Day 03

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -31,7 +31,7 @@ fn count_safe_reports(input: &str) -> usize {
             println!("report is {:?}", report);
             let mut safe = "true";
             for n in 0..(report.len()-1) {
-                if (report[n].abs_diff(report[n+1]) > 3 || report[n].abs_diff(report[n+1]) < 1){
+                if report[n].abs_diff(report[n+1]) > 3 || report[n].abs_diff(report[n+1]) < 1{
                     safe = "false";
                     }
                 }
@@ -42,6 +42,16 @@ fn count_safe_reports(input: &str) -> usize {
     println!("final count is {:?}", count);
     count
     }
+
+    // fn generate_dampened_reports(input: Vec<usize>) -> Vec<Vec<usize>> {
+    //     println!("input is {:?}", input);
+    //     let mut dampened_reports = vec![];
+    //     for i in 0..input.len()-1 {
+    //         dampened_reports.push(input.clone());
+    //         }
+    //     println!("dampened is {:?}", dampened_reports);
+    //     dampened_reports
+    //     }
 
 
 #[cfg(test)]
@@ -59,6 +69,10 @@ mod tests {
          1 3 6 7 9"
     }
 
+    fn test_input_2() -> Vec<usize> {
+        vec![7, 6, 4, 2, 1]
+    }
+
     #[rstest]
     fn test_parse_input(test_input: &str) {
         assert_eq!(parse_input(test_input), (vec![vec![7, 6, 4, 2, 1], vec![1, 2, 7, 8, 9], vec![9, 7, 6, 2, 1], vec![1, 3, 2, 4, 5], vec![8, 6, 4, 4, 1], vec![1, 3, 6, 7, 9]]));
@@ -68,5 +82,10 @@ mod tests {
     fn test_count_safe_reports(test_input: &str) {
         assert_eq!(count_safe_reports(test_input), 2)
         }
+
+    // #[rstest]
+    // fn test_generate_dampened_reports(test_input_2: Vec<usize>) {
+    //     assert_eq!(generate_dampened_reports(test_input_2), vec![vec![6,4,2,1], vec![7,4,2,1], vec![7,6,2,1], vec![7,6,4,2]])
+    //     }
 
 }
--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,12 +1,70 @@
+use std::borrow::Cow;
 use crate::{Solution, SolutionPair};
-use std::fs::read_to_string;
+use regex::Regex;
 
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let input = include_str!("input.txt");
+    let sol1 = get_result(input);
+    let sol2= get_result_2(input);
 
     (Solution::from(sol1), Solution::from(sol2))
+}
+
+fn parse_input(input: &str) -> Vec<(usize, usize)> {
+    let re = Regex::new(r"mul\((\d*),(\d*)\)").unwrap();
+    re.captures_iter(input).map(|m| (m[1].parse().unwrap(), m[2].parse().unwrap())).collect()
+}
+
+fn get_result(input: &str) -> usize {
+    let pairs = parse_input(input);
+    pairs.into_iter().map(|pair| pair.0 * pair.1).sum()
+}
+
+fn remove_dont_blocks(input: &str) -> Cow<str> {
+    let re = Regex::new(r"(?ms)don't\(\).*?do\(\)").unwrap();
+    re.replace_all(input, "")
+}
+
+fn get_result_2(input: &str) -> usize {
+    let new_input = remove_dont_blocks(input);
+    get_result(&new_input)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::*;
+
+    #[fixture]
+    fn test_input() -> &'static str {
+        "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    }
+
+    #[fixture]
+    fn test_input2() -> &'static str {
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    }
+
+    #[rstest]
+    fn test_parse_input(test_input: &str) {
+        assert_eq!(crate::days::day03::parse_input(test_input), (vec![(2,4), (5,5), (11,8), (8,5)]));
+    }
+
+    #[rstest]
+    fn test_get_result(test_input: &str) {
+        assert_eq!(crate::days::day03::get_result(test_input), 161);
+    }
+
+    #[rstest]
+    fn test_remove_dont_blocks(test_input2: &str) {
+        assert_eq!(crate::days::day03::remove_dont_blocks(test_input2), "xmul(2,4)&mul[3,7]!^?mul(8,5))");
+    }
+
+    #[rstest]
+    fn test_get_result_2(test_input2: &str) {
+        assert_eq!(crate::days::day03::get_result_2(test_input2), 48);
+    }
 }
